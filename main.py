2b02c54dddd8c54bf2a6543e685ae02fe160f837
sahelyromero <sahromeroca@unal.edu.co>
2023-08-18 22:35:31 -0500

    intento

--- main.py
+++ main.py
@@ -26,11 +26,10 @@ class Auto:
     
     cantidadCreados = 0
     
-    def __init__(self, modelo, precio, asientos, marca, motor, registro, cantidadCreados):
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asientos = asientos
         self.marca = marca
         self.motor = motor
         self.registro = registro
-        Auto.cantidadCreados = cantidadCreados
