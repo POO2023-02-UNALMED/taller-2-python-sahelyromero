7f32d84b6f0654bb50943134fea4b45a21655efb
sahelyromero <sahromeroca@unal.edu.co>
2023-08-18 22:18:36 -0500

    Clases motor y asiento

---
+++ main.py
@@ -0,0 +1,23 @@
+class Asiento:
+    
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if color == "amarillo" or color == "rojo" or color == "verde" or color == "negro" or color == "blanco":
+            self.color = color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if tipo == "electrico" or tipo == "gasolina":
+            self.tipo = tipo
